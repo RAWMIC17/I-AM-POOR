003b0097062a52e5b774b4fcdb1553f48228d2e3
RAWMIC17 <hks1710harsh@gmail.com>
2024-02-07 22:55:52 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 78,255,242),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 0, 66, 66),
+          title: "I am Poor".text.color(Vx.white).make().centered(),
+        ),
+        body:Center(child: Image(image: AssetImage("lib/assets/images/coal.png"),),), 
+      ),
+    );
+  }
+}
